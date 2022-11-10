0b7617b079318fa43eb736047ff13064ec32d760
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-11-10 17:29:05 +0100

    server mjs fix

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { env } from "../../../env/server.mjs";
+// import { env } from "../../../env/server.mjs";
 import { createContext } from "../../../server/trpc/context";
 import { appRouter } from "../../../server/trpc/router/_app";
 
@@ -8,10 +8,10 @@ import { appRouter } from "../../../server/trpc/router/_app";
 export default createNextApiHandler({
   router: appRouter,
   createContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(`❌ tRPC failed on ${path}: ${error}`);
-        }
-      : undefined,
+  // onError:
+  //   env.NODE_ENV === "development"
+  //     ? ({ path, error }) => {
+  //         console.error(`❌ tRPC failed on ${path}: ${error}`);
+  //       }
+  //     : undefined,
 });
